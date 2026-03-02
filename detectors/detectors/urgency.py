4997895286b3fa1cb4798575401a986b28c35048
dikshasriyam9-hash <dikshasriyam9@gmail.com>
2026-03-02 18:35:47 +0530

    Implement urgency detection in messages
    Add function to detect urgency in messages based on specific keywords.

--- detectors/detectors/urgency.py
+++ detectors/detectors/urgency.py
@@ -1,1 +1,17 @@
+urgency_words = [
+    "decide now",
+    "don't wait",
+    "hurry",
+    "urgent",
+    "quickly"
+]
 
+def detect_urgency(message):
+    score = 0
+    message = message.lower()
+
+    for word in urgency_words:
+        if word in message:
+            score += 1
+
+    return score
