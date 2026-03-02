2b4c8c5d4b8ac95eae807fb7aa96027d4e7c7b8b
dikshasriyam9-hash <dikshasriyam9@gmail.com>
2026-03-02 18:35:04 +0530

    Update secrecy.py

--- detectors/secrecy.py
+++ detectors/secrecy.py
@@ -1,1 +1,17 @@
+secrecy_words = [
+    "secret",
+    "don't tell",
+    "our secret",
+    "keep this private",
+    "no one should know"
+]
 
+def detect_secrecy(message):
+    score = 0
+    message = message.lower()
+
+    for word in secrecy_words:
+        if word in message:
+            score += 1
+
+    return score
