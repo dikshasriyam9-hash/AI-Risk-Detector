e601f8ca10aa9db03b7253d291136fee49eb8341
dikshasriyam9-hash <dikshasriyam9@gmail.com>
2026-03-02 18:50:39 +0530

    Add manipulation detection function and phrases

--- detectors/manipulation.py
+++ detectors/manipulation.py
@@ -1,1 +1,17 @@
+manipulation_phrases = [
+    "if you loved me",
+    "prove your love",
+    "you don't trust me",
+    "you are overthinking",
+    "only you understand me"
+]
 
+def detect_manipulation(message):
+    score = 0
+    message = message.lower()
+
+    for phrase in manipulation_phrases:
+        if phrase in message:
+            score += 1
+
+    return score
